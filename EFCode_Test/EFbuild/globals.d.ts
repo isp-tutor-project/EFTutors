bc0b5d047ed1d287e106e65352d800c256377e7b
synaptek <KevinWillows@gmail.com>
2018-07-31 17:09:33 -0400

    Support for $Globals

---
+++ EFCode_Test/EFbuild/globals.d.ts
@@ -0,0 +1,19 @@
+declare namespace EFTut_Suppl.$GLOBAL {
+    class CONST {
+        static readonly TUTORCONTAINER: string;
+        static readonly NEXTSCENE: string;
+        static readonly PREVSCENE: string;
+        static readonly MOUSE_MOVE: string;
+        static readonly MOUSE_DOWN: string;
+        static readonly MOUSE_UP: string;
+        static readonly MOUSE_CLICK: string;
+        static readonly DOUBLE_CLICK: string;
+        static readonly CLICK: string;
+    }
+}
+declare namespace EFTut_Suppl.$GLOBAL {
+    class $GLOBAL {
+        $var1: string;
+        [key: string]: any;
+    }
+}
