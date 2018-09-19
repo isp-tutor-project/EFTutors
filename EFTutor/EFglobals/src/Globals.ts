885511db2a25700181e8da800a9651aa76465610
synaptek <KevinWillows@gmail.com>
2018-09-19 11:22:21 -0400

    Global Mixins RQ

--- EFTutor/EFglobals/src/Globals.ts
+++ EFTutor/EFglobals/src/Globals.ts
@@ -1,13 +1,40 @@
 namespace EFTut_Suppl.$GLOBAL {
 
+    // NOTE: This executes in the TutorDoc scope
+    // 
     export class $GLOBAL {
-
-        $var1:string = "valname2";
 
         // This is a special signature to avoid the typescript error "because <type> has no index signature."
         // on syntax like => this[<element name>]
         // 
         [key: string]: any;
        
+        // Global Tutor Graph Constraints
+        // 
+        public $nodeConstraint(constrainId:string) : boolean {
+
+            let result:boolean = false;
+
+            let RQconf = this.getModuleValue("RQconfirmation");       
+
+            switch(constrainId) {
+
+                case "CHANGE_RQ":
+                    if(RQconf === constrainId)
+                                        result = true;
+                    break;
+                case "CHANGE_TOPIC":
+                    if(RQconf === constrainId)
+                                        result = true;
+                    break;
+                case "CHANGE_AREA":
+                    if(RQconf === constrainId)
+                                        result = true;
+                    break;
+            }
+
+            return result;
+        }
+
    }
 }
