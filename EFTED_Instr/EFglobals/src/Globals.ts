a46d6239d8623eff7a8103646a0d581ba8a05cad
synaptek <KevinWillows@gmail.com>
2018-09-27 19:34:48 -0400

    TED wip

--- EFTED_Instr/EFglobals/src/Globals.ts
+++ EFTED_Instr/EFglobals/src/Globals.ts
@@ -1,13 +1,27 @@
 namespace EFTut_Suppl.$GLOBAL {
 
+    // These mixins execute within the CTutorDoc scope
+    // 
     export class $GLOBAL {
 
-        $var1:string = "valname2";
 
         // This is a special signature to avoid the typescript error "because <type> has no index signature."
         // on syntax like => this[<element name>]
         // 
         [key: string]: any;
        
-   }
+
+        public $nodeConstraint(constrainId:string) : boolean {
+
+            let result:boolean = false;
+
+            switch(constrainId) {
+                default:
+                    result = this.testFeatures(constrainId);           
+                    break;
+            }
+
+            return result;
+        }
+    }
 }
