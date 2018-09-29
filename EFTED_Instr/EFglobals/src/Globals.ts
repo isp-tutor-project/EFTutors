084e3779370f3034e4c900a20af60e1e4492b97c
synaptek <KevinWillows@gmail.com>
2018-09-29 17:20:52 -0400

    TED wip

--- EFTED_Instr/EFglobals/src/Globals.ts
+++ EFTED_Instr/EFglobals/src/Globals.ts
@@ -11,13 +11,53 @@ namespace EFTut_Suppl.$GLOBAL {
         [key: string]: any;
        
 
-        public $nodeConstraint(constrainId:string) : boolean {
+        public $nodeConstraint(nodeId:string, constrainId:string) : boolean {
 
             let result:boolean = false;
 
+            switch(nodeId) {
+                case "TED_INTRO":
+                    result = this.testFeatures(constrainId);           
+                    break;
+
+                case "TEDQ1":
+                    switch(constrainId) {
+
+                        case "CORRECT":
+                            result = this.getModuleValue("Expt1_Q1").value;                                        
+                            break;
+
+                        case "INCORRECT":
+                            result = !this.getModuleValue("Expt1_Q1").value;                                        
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "TEDQ4":
+                    switch(constrainId) {
+                        
+                        case "CORRECT":
+                            result = this.getModuleValue("Expt1_Q4").value;                                        
+                            break;
+
+                        case "INCORRECT":
+                            result = !this.getModuleValue("Expt1_Q4").value;                                        
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                default: 
+                    break;
+            }
+
             switch(constrainId) {
                 default:
-                    result = this.testFeatures(constrainId);           
                     break;
             }
 
