4352486d35ae1d8489d9177fdedd311de996240c
synaptek <KevinWillows@gmail.com>
2019-06-18 11:09:20 -0400

    Conditional Even Question path

--- EFTED_PrePost/EFglobals/src/Globals.ts
+++ EFTED_PrePost/EFglobals/src/Globals.ts
@@ -15,5 +15,33 @@ namespace EFTut_Suppl.$GLOBAL {
             scene.setBreadCrumbs(scene.name);
         }        
 
+
+        // Global Tutor Graph Constraints
+        // 
+        public $nodeConstraint(nodeId:string, constraintId:string) : boolean {
+
+            let result:boolean = false;
+
+            switch(constraintId) {
+
+                case "Q2BAD_SELECTED":
+                    if(this.getModuleValue('q2Bad') === "checked")
+                                                        result = true;
+                    break;
+
+                case "Q4BAD_SELECTED":
+                    if(this.getModuleValue('q4Bad') === "checked")
+                                                        result = true;
+                    break;
+
+                case "Q6BAD_SELECTED":
+                    if(this.getModuleValue('q6Bad') === "checked")
+                                                        result = true;
+                    break;
+            }
+
+            return result;
+        }
+
    }
 }
